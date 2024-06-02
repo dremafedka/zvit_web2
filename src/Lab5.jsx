4c9cddd551fe1ca2cbfc1930367531bea1c3a075
dremafedka <fedorgorbenko@gmail.com>
2024-06-03 00:20:09 +0300

    updated to 5.1, 8 lab

--- src/Lab5.jsx
+++ src/Lab5.jsx
@@ -7,6 +7,18 @@ import lab5_4 from './assets/lab5_4.png'
 import lab5_5 from './assets/lab5_5.png'
 import lab5_6 from './assets/lab5_6.png'
 import lab5_7 from './assets/lab5_7.png'
+import img1 from './assets/lab5/img.png'
+import img2 from './assets/lab5/img_1.png'
+import img3 from './assets/lab5/img_2.png'
+import img4 from './assets/lab5/img_3.png'
+import img5 from './assets/lab5/img_4.png'
+import img6 from './assets/lab5/img_5.png'
+import img7 from './assets/lab5/img_6.png'
+import img8 from './assets/lab5/img_7.png'
+import img9 from './assets/lab5/img_8.png'
+import img10 from './assets/lab5/img_9.png'
+import img11 from './assets/lab5/img_10.png'
+import img12 from './assets/lab5/img_11.png'
 
 
 export const Lab5 = () => {
@@ -73,6 +85,66 @@ export const Lab5 = () => {
                 </div>
             );
             break;
+        case "Лаба 5.1":
+            content = (
+                <div>
+                    <h1>Завдання 2</h1>
+                    <span style={{fontSize: "40px"}}>Вигляд програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img1} alt=""/>
+                    <br/>
+                    <span style={{fontSize: "40px"}}>Код програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img2} alt=""/>
+                    <br/>
+                    <h1>Завдання 4</h1>
+                    <span style={{fontSize: "40px"}}>Вигляд програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img3} alt=""/>
+                    <br/>
+                    <span style={{fontSize: "40px"}}>Код програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img4} alt=""/>
+                    <br/>
+                    <h1>Завдання 6</h1>
+                    <span style={{fontSize: "40px"}}>Вигляд програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img5} alt=""/>
+                    <br/>
+                    <span style={{fontSize: "40px"}}>Код програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img6} alt=""/>
+                    <br/>
+                    <h1>Завдання 8</h1>
+                    <span style={{fontSize: "40px"}}>Вигляд програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img7} alt=""/>
+                    <br/>
+                    <span style={{fontSize: "40px"}}>Код програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img8} alt=""/>
+                    <br/>
+                    <h1>Завдання 9</h1>
+                    <span style={{fontSize: "40px"}}>Вигляд програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img9} alt=""/>
+                    <br/>
+                    <span style={{fontSize: "40px"}}>Код програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img10} alt=""/>
+                    <br/>
+                    <h1>Завдання 10</h1>
+                    <span style={{fontSize: "40px"}}>Вигляд програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img11} alt=""/>
+                    <br/>
+                    <span style={{fontSize: "40px"}}>Код програми</span>
+                    <br/>
+                    <img style={{ width: "400px" }} src={img12} alt=""/>
+                    <br/>
+                </div>
+            );
+            break;
         case "Висновки":
             content = <div>
                 <h1>Висновки по роботі над проєктом</h1>
@@ -98,6 +170,7 @@ export const Lab5 = () => {
                     <div className={styles.menu_subitem} onClick={() => handleMenuItemClick("Вставка (тег SCRIPT)")}>Вставка (тег SCRIPT)</div>
                     <div className={styles.menu_subitem} onClick={() => handleMenuItemClick("Будильник (6 пункт)")}>Будильник (6 пункт)</div>
                 </div>
+                <div className={styles.menu_item} onClick={() => handleMenuItemClick("Лаба 5.1")}>Лаба 5.1</div>
                 <div className={styles.menu_item} onClick={() => handleMenuItemClick("Висновки")}>Висновки</div>
             </div>
             <div className={styles.lab_content}>
--- src/Lab8.jsx
+++ src/Lab8.jsx
@@ -34,7 +34,21 @@ import lab7_34 from './assets/photo_29_2024-05-28_18-37-14.jpg';
 import lab7_35 from './assets/photo_30_2024-05-28_18-37-14.jpg';
 import lab7_36 from './assets/photo_31_2024-05-28_18-37-14.jpg';
 import lab7_37 from './assets/photo_32_2024-05-28_18-37-14.jpg';
-
+import img1 from './assets/lab8/photo_1_2024-06-02_23-30-04.jpg';
+import img2 from './assets/lab8/photo_1_2024-06-02_23-30-04.jpg';
+import img3 from './assets/lab8/photo_1_2024-06-02_23-35-45.jpg';
+import img4 from './assets/lab8/photo_2_2024-06-02_23-35-45.jpg';
+import img5 from './assets/lab8/photo_3_2024-06-02_23-35-45.jpg';
+import img6 from './assets/lab8/photo_1_2024-06-02_23-39-24.jpg';
+import img7 from './assets/lab8/photo_2_2024-06-02_23-39-24.jpg';
+import img8 from './assets/lab8/photo_3_2024-06-02_23-39-24.jpg';
+import img9 from './assets/lab8/photo_1_2024-06-02_23-41-23.jpg';
+import img10 from './assets/lab8/photo_1_2024-06-02_23-42-49.jpg';
+import img11 from './assets/lab8/photo_1_2024-06-02_23-42-49.jpg';
+import img12 from './assets/lab8/photo_2_2024-06-02_23-42-49.jpg';
+import img13 from './assets/lab8/photo_3_2024-06-02_23-42-49.jpg';
+import img14 from './assets/lab8/photo_4_2024-06-02_23-42-49.jpg';
+import img15 from './assets/lab8/photo_5_2024-06-02_23-42-49.jpg';
 
 
 export const Lab8 = () => {
@@ -133,6 +147,50 @@ export const Lab8 = () => {
                 </div>
             );
             break;
+        case "Лаби":
+            content = (
+                <div>
+                    <h1>Лаба 1</h1>
+                    <img style={{ width: "600px" }} src={img1} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img2} alt=""/>
+                    <br/>
+                    <h1>Лаба 2</h1>
+                    <img style={{ width: "600px" }} src={img3} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img4} alt=""/>
+                    <br/>
+                    <h1>Лаба 3</h1>
+                    <img style={{ width: "600px" }} src={img5} alt=""/>
+                    <br/>
+                    <h1>Лаба 4</h1>
+                    <img style={{ width: "600px" }} src={img6} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img7} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img8} alt=""/>
+                    <br/>
+                    <h1>Лаба 5</h1>
+                    <img style={{ width: "600px" }} src={img9} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img10} alt=""/>
+                    <br/>
+                    <h1>Лаба 6</h1>
+                    <img style={{ width: "600px" }} src={img11} alt=""/>
+                    <br/>
+                    <h1>Лаба 7</h1>
+                    <img style={{ width: "600px" }} src={img12} alt=""/>
+                    <br/>
+                    <h1>Лаба 8</h1>
+                    <img style={{ width: "600px" }} src={img13} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img14} alt=""/>
+                    <br/>
+                    <img style={{ width: "600px" }} src={img15} alt=""/>
+                    <br/>
+                </div>
+            );
+            break;
         default:
             content = null;
     }
@@ -143,6 +201,7 @@ export const Lab8 = () => {
                 <div className={styles.menu_item} onClick={() => handleMenuItemClick("Оцінки")}>Оцінки</div>
                 <div className={styles.menu_item} onClick={() => handleMenuItemClick("Сертифікат")}>Сертифікат</div>
                 <div className={styles.menu_item} onClick={() => handleMenuItemClick("Тести")}>Тести</div>
+                <div className={styles.menu_item} onClick={() => handleMenuItemClick("Лаби")}>Лаби</div>
             </div>
             <div className={styles.lab_content}>
                 {content}
